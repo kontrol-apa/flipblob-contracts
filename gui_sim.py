a4b40a7abc1db671376d8ee45ce556288d956746
partychad <partychad@protonmail.com>
2023-08-23 22:04:35 +0200

    added a basic GUI to set parameters

---
+++ gui_sim.py
@@ -0,0 +1,74 @@
+# gui_casino_simulation.py
+
+import tkinter as tk
+from tkinter import ttk
+import concurrent.futures
+import sim as cs
+def update_label(value, label_var):
+    label_var.set(int(float(value)))
+    
+def update_and_plot():
+    params = {
+        "NUM_SIMULATIONS": int(num_sims_slider.get()),
+        "NUM_FLIPS": int(num_flips_slider.get()),
+        "START_TREASURY": int(start_treasury_slider.get()),
+        "HOUSE_EDGE": house_edge_slider.get(),
+        "NETWORK_COST": network_cost_slider.get()
+    }
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(executor.map(cs.simulate, range(params["NUM_SIMULATIONS"])))
+
+    cs.plot_results(params["NUM_SIMULATIONS"], params["NUM_FLIPS"], params["START_TREASURY"], params["HOUSE_EDGE"], params["NETWORK_COST"], results)
+
+def update_label(slider, label_var):
+    label_var.set(int(slider.get()))
+
+root = tk.Tk()
+root.title("Casino Simulator")
+
+frame = ttk.Frame(root)
+frame.grid(padx=20, pady=20)
+
+# Label Variables for the sliders
+num_sims_var = tk.StringVar()
+num_flips_var = tk.StringVar()
+start_treasury_var = tk.StringVar()
+
+# GUI Components
+ttk.Label(frame, text="Number of Simulations:").grid(row=0, column=0, sticky=tk.W)
+num_sims_slider = ttk.Scale(frame, from_=10, to_=500, orient=tk.HORIZONTAL, length=300, command=lambda e: update_label(num_sims_slider, num_sims_var))
+num_sims_slider.set(cs.NUM_SIMULATIONS)
+num_sims_slider.grid(row=0, column=1)
+ttk.Label(frame, textvariable=num_sims_var).grid(row=0, column=2)
+num_sims_var.set(cs.NUM_SIMULATIONS)
+
+ttk.Label(frame, text="Number of Flips:").grid(row=1, column=0, sticky=tk.W)
+num_flips_slider = ttk.Scale(frame, from_=1000, to_=50000, orient=tk.HORIZONTAL, length=300, command=lambda e: update_label(num_flips_slider, num_flips_var))
+num_flips_slider.set(cs.NUM_FLIPS)
+num_flips_slider.grid(row=1, column=1)
+ttk.Label(frame, textvariable=num_flips_var).grid(row=1, column=2)
+num_flips_var.set(cs.NUM_FLIPS)
+
+ttk.Label(frame, text="Starting Treasury:").grid(row=2, column=0, sticky=tk.W)
+start_treasury_slider = ttk.Scale(frame, from_=1000, to_=20000, orient=tk.HORIZONTAL, length=300, command=lambda e: update_label(start_treasury_slider, start_treasury_var))
+start_treasury_slider.set(cs.START_TREASURY)
+start_treasury_slider.grid(row=2, column=1)
+ttk.Label(frame, textvariable=start_treasury_var).grid(row=2, column=2)
+start_treasury_var.set(cs.START_TREASURY)
+
+ttk.Label(frame, text="House Edge:").grid(row=3, column=0)
+house_edge_slider = tk.Scale(frame, from_=0.01, to_=0.1, orient=tk.HORIZONTAL, length=300, resolution=0.01)
+house_edge_slider.set(cs.HOUSE_EDGE)
+house_edge_slider.grid(row=3, column=1)
+
+ttk.Label(frame, text="Network Cost:").grid(row=4, column=0)
+network_cost_slider = tk.Scale(frame, from_=0.1, to_=5, orient=tk.HORIZONTAL, length=300, resolution=0.1)
+network_cost_slider.set(cs.NETWORK_COST)
+network_cost_slider.grid(row=4, column=1)
+
+run_button = ttk.Button(frame, text="Run Simulation & Plot", command=update_and_plot)
+run_button.grid(row=5, columnspan=2, pady=20)
+
+root.mainloop()
+
--- sim.py
+++ sim.py
@@ -37,68 +37,69 @@ def simulate(_):
         
     return balances, lowest_balance, wins, losses, volumes, (treasury - START_TREASURY)
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    results = list(executor.map(simulate, range(NUM_SIMULATIONS)))
-
-# Unpack the results into their individual components
-balances, lowest_balances, wins, losses, volumes, profits = zip(*results)
-
-bankruptcy_count = sum(1 for balance in lowest_balances if balance <= 0)
-# Average balances over all simulations
-average_results = [sum(x) / NUM_SIMULATIONS for x in zip(*balances)]
-average_volume = sum(volumes) / NUM_SIMULATIONS 
-average_profit = sum(profits) / NUM_SIMULATIONS 
 
 
-fig, axs = plt.subplots(4, 1, figsize=(10,15))
+def plot_results(NUM_SIMULATIONS, NUM_FLIPS, START_TREASURY, HOUSE_EDGE, NETWORK_COST, results):
 
-# Average treasury balance over time
-axs[0].plot(average_results, color="blue", label="Average Treasury")
-axs[0].set_title(f"Average Treasury Balance Over Time\n({NUM_SIMULATIONS} Parallel Simulations, {NUM_FLIPS} Plays Each)")
-axs[0].set_xlabel("Flips")
-axs[0].set_ylabel("Balance")
-axs[0].legend()
+    # Unpack the results into their individual components
+    balances, lowest_balances, wins, losses, volumes, profits = zip(*results)
 
-# Lowest Treasury per Run
-axs[1].scatter(range(NUM_SIMULATIONS), lowest_balances, color="red", label="Lowest Treasury per Run")
-axs[1].set_title(f"Lowest Treasury per Run\n({NUM_SIMULATIONS} Parallel Simulations, {NUM_FLIPS} Plays Each)")
-axs[1].set_xlabel("Simulation Run")
-axs[1].set_ylabel("Lowest Balance")
-axs[1].legend()
-
-# Wins and Losses per Run
-axs[2].bar(range(NUM_SIMULATIONS), wins, color="green", label="Wins")
-axs[2].bar(range(NUM_SIMULATIONS), losses, color="red", label="Losses", bottom=wins)
-axs[2].set_title(f"Wins and Losses per Run\n({NUM_SIMULATIONS} Parallel Simulations, {NUM_FLIPS} Plays Each)")
-axs[2].set_xlabel("Simulation Run")
-axs[2].set_ylabel("Count")
-axs[2].legend()
+    bankruptcy_count = sum(1 for balance in lowest_balances if balance <= 0)
+    # Average balances over all simulations
+    average_results = [sum(x) / NUM_SIMULATIONS for x in zip(*balances)]
+    average_volume = sum(volumes) / NUM_SIMULATIONS 
+    average_profit = sum(profits) / NUM_SIMULATIONS 
 
 
-font = FontProperties()
-font.set_family('monospace')
-font.set_size(9)
-font.set_weight('bold')
+    fig, axs = plt.subplots(4, 1, figsize=(10,15))
 
-axs[3].text(0.5, 0.9, f'Starting Treasury: ${START_TREASURY:.0f},  Bet Options: {BET_OPTIONS}', 
-            ha='center', va='center', fontproperties=font)
-axs[3].text(0.5, 0.8, f'House Edge: {HOUSE_EDGE*100}%,  Network Cost Per TX: ${NETWORK_COST}', 
-            ha='center', va='center', fontproperties=font)
-axs[3].axis('off') # Turn off axis
-font.set_size(14)
-axs[3].text(0.5, 0.6, f'Average Volume: ${average_volume:.0f}', 
-            ha='center', va='center', fontproperties=font)
-axs[3].text(0.5, 0.5, f'Bankruptcy: {bankruptcy_count:.0f}', 
-            ha='center', va='center', fontproperties=font)
-axs[3].text(0.5, 0.4, f'Average Profit: ${average_profit:.0f}', 
-            ha='center', va='center', fontproperties=font)
+    # Average treasury balance over time
+    axs[0].plot(average_results, color="blue", label="Average Treasury")
+    axs[0].set_title(f"Average Treasury Balance Over Time\n({NUM_SIMULATIONS} Parallel Simulations, {NUM_FLIPS} Plays Each)")
+    axs[0].set_xlabel("Flips")
+    axs[0].set_ylabel("Balance")
+    axs[0].legend()
+
+    # Lowest Treasury per Run
+    axs[1].scatter(range(NUM_SIMULATIONS), lowest_balances, color="red", label="Lowest Treasury per Run")
+    axs[1].set_title(f"Lowest Treasury per Run\n({NUM_SIMULATIONS} Parallel Simulations, {NUM_FLIPS} Plays Each)")
+    axs[1].set_xlabel("Simulation Run")
+    axs[1].set_ylabel("Lowest Balance")
+    axs[1].legend()
+
+    # Wins and Losses per Run
+    axs[2].bar(range(NUM_SIMULATIONS), wins, color="green", label="Wins")
+    axs[2].bar(range(NUM_SIMULATIONS), losses, color="red", label="Losses", bottom=wins)
+    axs[2].set_title(f"Wins and Losses per Run\n({NUM_SIMULATIONS} Parallel Simulations, {NUM_FLIPS} Plays Each)")
+    axs[2].set_xlabel("Simulation Run")
+    axs[2].set_ylabel("Count")
+    axs[2].legend()
 
 
+    font = FontProperties()
+    font.set_family('monospace')
+    font.set_size(9)
+    font.set_weight('bold')
 
-plt.tight_layout()
-plt.show()
+    axs[3].text(0.5, 0.9, f'Starting Treasury: ${START_TREASURY:.0f},  Bet Options: {BET_OPTIONS}', 
+                ha='center', va='center', fontproperties=font)
+    axs[3].text(0.5, 0.8, f'House Edge: {HOUSE_EDGE*100}%,  Network Cost Per TX: ${NETWORK_COST}', 
+                ha='center', va='center', fontproperties=font)
+    axs[3].axis('off') # Turn off axis
+    font.set_size(14)
+    axs[3].text(0.5, 0.6, f'Average Volume: ${average_volume:.0f}', 
+                ha='center', va='center', fontproperties=font)
+    axs[3].text(0.5, 0.5, f'Bankruptcy: {bankruptcy_count:.0f}', 
+                ha='center', va='center', fontproperties=font)
+    axs[3].text(0.5, 0.4, f'Average Profit: ${average_profit:.0f}', 
+                ha='center', va='center', fontproperties=font)
 
 
 
-# TOTAL VOLUME
-# TOTAL PROFIT
+    plt.tight_layout()
+    plt.show()
+
+
+
+    # TOTAL VOLUME
+    # TOTAL PROFIT
