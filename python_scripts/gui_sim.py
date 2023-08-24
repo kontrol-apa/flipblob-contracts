25984acb4a68bf024281f0bc112a168971c57e2b
partychad <96027087+partychad@users.noreply.github.com>
2023-08-24 14:19:15 +0200

    Scripts (#6)
    * updated gitignore
    
    * initial version of the script
    
    * added a text field below
    
    * added profit and banktrupcy information
    
    * added a informative text section below
    
    * added a basic GUI to set parameters
    
    * fixed a bug where parameters werent propogated to the logic layer
    
    * integrated the graph and the gui together
    
    * removed redundant code and reduced the gui size so it fits the 14" display
    
    * house edge is an integer between 0 and 100 now
    
    * added save and exit buttons decreased the canvas size
    
    * updated gitignore
    
    * added unique name to save function
    
    * reorganized folders

---
+++ python_scripts/gui_sim.py
@@ -0,0 +1,134 @@
+# gui_casino_simulation.py
+import tkinter as tk
+from tkinter import ttk
+import concurrent.futures
+import sim as cs
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import hashlib
+import os
+
+current_directory = os.getcwd()
+
+def update_label(value, label_var):
+    label_var.set(int(float(value)))
+    
+def update_and_plot():
+    
+    cs.NUM_SIMULATIONS = int(num_sims_slider.get())
+    cs.NUM_FLIPS = int(num_flips_slider.get())
+    cs.START_TREASURY = int(start_treasury_slider.get())
+    cs.HOUSE_EDGE = house_edge_slider.get()/100
+    cs.NETWORK_COST = network_cost_slider.get()
+    
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(executor.map(cs.simulate, range(cs.NUM_SIMULATIONS)))
+
+    cs.plot_results( cs.NUM_SIMULATIONS, cs.NUM_FLIPS, cs.START_TREASURY, cs.HOUSE_EDGE, cs.NETWORK_COST, results)
+
+    for widget in plot_frame.winfo_children():
+        widget.destroy()
+
+    canvas = FigureCanvasTkAgg(cs.fig, master=plot_frame)  # Create the canvas
+    canvas_widget = canvas.get_tk_widget()
+    canvas_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    canvas.draw()
+
+def update_label(slider, label_var):
+    label_var.set(int(slider.get()))
+
+
+def save_plot():
+    file_path = save_location_var.get()
+
+    parameter_hash = hashlib.md5(str(cs.NUM_SIMULATIONS + cs.NUM_FLIPS + cs.START_TREASURY + cs.HOUSE_EDGE + cs.NETWORK_COST).encode()).hexdigest()
+
+    # Construct the file name using the hash
+    file_name = f"simulation_{parameter_hash}.png"
+
+    # Construct the full path
+    file_path = os.path.join(current_directory, file_name)
+
+    if not file_path:
+        return  # Don't save if the file path is empty
+
+    if not file_path.endswith('.png'):
+        file_path += '.png'  # Append .png extension if not provided
+
+    canvas = cs.fig.canvas
+    canvas.print_png(file_path)
+    print(f"Plot saved as {file_path}")
+
+def exit_program():
+    print("Exiting.")
+    root.destroy()
+    exit()
+
+root = tk.Tk()
+root.title("Casino Simulator")
+
+frame = ttk.Frame(root)
+frame.grid(padx=20, pady=20)
+
+# Label Variables for the sliders
+num_sims_var = tk.StringVar()
+num_flips_var = tk.StringVar()
+start_treasury_var = tk.StringVar()
+house_edge_var = tk.StringVar()
+
+# GUI Components
+ttk.Label(frame, text="Number of Simulations:").grid(row=0, column=0, sticky=tk.W)
+num_sims_slider = ttk.Scale(frame, from_=10, to_=500, orient=tk.HORIZONTAL, length=300, command=lambda e: update_label(num_sims_slider, num_sims_var))
+num_sims_slider.set(cs.NUM_SIMULATIONS)
+num_sims_slider.grid(row=0, column=1)
+ttk.Label(frame, textvariable=num_sims_var).grid(row=0, column=2)
+num_sims_var.set(cs.NUM_SIMULATIONS)
+
+ttk.Label(frame, text="Number of Flips:").grid(row=1, column=0, sticky=tk.W)
+num_flips_slider = ttk.Scale(frame, from_=1000, to_=50000, orient=tk.HORIZONTAL, length=300, command=lambda e: update_label(num_flips_slider, num_flips_var))
+num_flips_slider.set(cs.NUM_FLIPS)
+num_flips_slider.grid(row=1, column=1)
+ttk.Label(frame, textvariable=num_flips_var).grid(row=1, column=2)
+num_flips_var.set(cs.NUM_FLIPS)
+
+ttk.Label(frame, text="Starting Treasury:").grid(row=2, column=0, sticky=tk.W)
+start_treasury_slider = ttk.Scale(frame, from_=1000, to_=20000, orient=tk.HORIZONTAL, length=300, command=lambda e: update_label(start_treasury_slider, start_treasury_var))
+start_treasury_slider.set(cs.START_TREASURY)
+start_treasury_slider.grid(row=2, column=1)
+ttk.Label(frame, textvariable=start_treasury_var).grid(row=2, column=2)
+start_treasury_var.set(cs.START_TREASURY)
+
+ttk.Label(frame, text="House Edge:").grid(row=3, column=0)
+house_edge_slider = tk.Scale(frame, from_=0, to_=100, orient=tk.HORIZONTAL, length=300, command=lambda e: update_label(house_edge_slider, house_edge_var))
+house_edge_slider.set(cs.HOUSE_EDGE)
+house_edge_slider.grid(row=3, column=1)
+
+ttk.Label(frame, text="Network Cost:").grid(row=4, column=0)
+network_cost_slider = tk.Scale(frame, from_=0.1, to_=5, orient=tk.HORIZONTAL, length=300, resolution=0.1)
+network_cost_slider.set(cs.NETWORK_COST)
+network_cost_slider.grid(row=4, column=1)
+
+run_button = ttk.Button(frame, text="Run", command=update_and_plot)
+run_button.grid(row=5, columnspan=2, pady=20)
+
+plot_frame = ttk.Frame(root)
+plot_frame.grid(row=6, columnspan=3, padx=20, pady=20)
+
+
+save_frame = ttk.Frame(root)
+save_frame.grid(row=7, columnspan=3, padx=20, pady=10)
+
+save_location_var = tk.StringVar(value=current_directory)  # Default save location
+ttk.Label(save_frame, text="Save:").grid(row=0, column=0, sticky=tk.W)
+save_location_entry = ttk.Entry(save_frame, textvariable=save_location_var)
+save_location_entry.grid(row=0, column=1, padx=5)
+
+save_button = ttk.Button(save_frame, text="Save Plot", command=save_plot)
+save_button.grid(row=0, column=2, padx=5)
+
+exit_button = ttk.Button(root, text="Exit", command=exit_program)
+exit_button.grid(row=8, columnspan=3, pady=10)
+
+
+root.mainloop()
+
---
+++ python_scripts/sim.py
@@ -0,0 +1,106 @@
+import random
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+
+# fig = plt.figure()
+
+NUM_SIMULATIONS = 100
+NUM_FLIPS = 10000
+START_TREASURY = 5000
+MAX_BET = 100
+BET_OPTIONS = [5, 10, 20, 50, 100]
+HOUSE_EDGE = 5
+NETWORK_COST = 0.5
+
+def simulate(_):
+    treasury = START_TREASURY
+    wins, losses, volumes = 0, 0, 0
+    lowest_balance = START_TREASURY
+    balances = [START_TREASURY]
+     
+    for _ in range(NUM_FLIPS):
+        bet = random.choice(BET_OPTIONS)
+        volumes += bet
+        if bet > treasury:
+            lowest_balance = 0
+            break
+        coin_flip = random.choice([True, False]) # True is win, False is lose
+        if coin_flip:
+            payout = bet * (1 - HOUSE_EDGE)
+            treasury -= payout
+            wins += 1
+        else:
+            treasury += bet
+            losses += 1
+        treasury -= NETWORK_COST
+        balances.append(treasury)
+        lowest_balance = min(lowest_balance, treasury)
+
+        
+    return balances, lowest_balance, wins, losses, volumes, (treasury - START_TREASURY)
+
+
+
+def plot_results(NUM_SIMULATIONS, NUM_FLIPS, START_TREASURY, HOUSE_EDGE, NETWORK_COST, results):
+
+    global fig  # Declare fig as global so it can be accessed from outside the function
+    # Unpack the results into their individual components
+    balances, lowest_balances, wins, losses, volumes, profits = zip(*results)
+
+    bankruptcy_count = sum(1 for balance in lowest_balances if balance <= 0)
+    # Average balances over all simulations
+    average_results = [sum(x) / NUM_SIMULATIONS for x in zip(*balances)]
+    average_volume = sum(volumes) / NUM_SIMULATIONS 
+    average_profit = sum(profits) / NUM_SIMULATIONS 
+
+
+    fig, axs = plt.subplots(4, 1, figsize=(5,10))
+
+    # Average treasury balance over time
+    axs[0].plot(average_results, color="blue", label="Average Treasury")
+    axs[0].set_title(f"Average Treasury Balance Over Time\n({NUM_SIMULATIONS} Parallel Simulations, {NUM_FLIPS} Plays Each)")
+    axs[0].set_xlabel("Flips")
+    axs[0].set_ylabel("Balance")
+    axs[0].legend()
+
+    # Lowest Treasury per Run
+    axs[1].scatter(range(NUM_SIMULATIONS), lowest_balances, color="red", label="Lowest Treasury per Run")
+    axs[1].set_title(f"Lowest Treasury per Run\n({NUM_SIMULATIONS} Parallel Simulations, {NUM_FLIPS} Plays Each)")
+    axs[1].set_xlabel("Simulation Run")
+    axs[1].set_ylabel("Lowest Balance")
+
+    # Wins and Losses per Run
+    axs[2].bar(range(NUM_SIMULATIONS), wins, color="green", label="Wins")
+    axs[2].bar(range(NUM_SIMULATIONS), losses, color="red", label="Losses", bottom=wins)
+    axs[2].set_title(f"Wins and Losses per Run\n({NUM_SIMULATIONS} Parallel Simulations, {NUM_FLIPS} Plays Each)")
+    axs[2].set_xlabel("Simulation Run")
+    axs[2].set_ylabel("Count")
+    axs[2].legend()
+
+
+    font = FontProperties()
+    font.set_family('monospace')
+    font.set_size(9)
+    font.set_weight('bold')
+
+    axs[3].text(0.5, 0.9, f'Starting Treasury: ${START_TREASURY:.0f},  Bet Options: {BET_OPTIONS}', 
+                ha='center', va='center', fontproperties=font)
+    axs[3].text(0.5, 0.8, f'House Edge: {HOUSE_EDGE*100}%,  Network Cost Per TX: ${NETWORK_COST}', 
+                ha='center', va='center', fontproperties=font)
+    axs[3].axis('off') # Turn off axis
+    font.set_size(14)
+    axs[3].text(0.5, 0.6, f'Average Volume: ${average_volume:.0f}', 
+                ha='center', va='center', fontproperties=font)
+    axs[3].text(0.5, 0.4, f'Bankruptcy: {bankruptcy_count:.0f}', 
+                ha='center', va='center', fontproperties=font)
+    axs[3].text(0.5, 0.2, f'Average Profit: ${average_profit:.0f}', 
+                ha='center', va='center', fontproperties=font)
+
+
+
+    plt.tight_layout()
+
+
+
+    # TOTAL VOLUME
+    # TOTAL PROFIT
