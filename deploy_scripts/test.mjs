d3ef198deb3faac8ddf4f7de07a97f1ae5cbf66c
partychad <partychad@protonmail.com>
2023-11-26 15:08:10 +0100

    updated teh getCOnfig function with infura key injection

--- deploy_scripts/test.mjs
+++ deploy_scripts/test.mjs
@@ -1,5 +1,5 @@
 
-import { Contract, Account, constants, CallData, RpcProvider } from 'starknet';
+import { Contract, Account, constants, RpcProvider } from 'starknet';
 import { shortString } from 'starknet';
 import fs from 'fs';
 import { cairo } from "starknet";
@@ -7,6 +7,7 @@ import { cairo } from "starknet";
 const tokenWETHAddress = '0x034e31357d1c3693bda06d04bf4c51557514ECed5A8e9973bDb772f7fB978B36'
 const flipTestAddress = "0x02b1512db3cc59de4e13f7f9900ac399303c707c9969be4f8458b1b5bde63ee9";
 const tokenWETHIdentifier = "WETH";
+const finalizer = '0x551e361b1f456856968d00e2ea991daecc04eed605903030ffbc547616da258'
 
 
 const { privateKey, accountAddress, provider } = getConfig("TESTNET");
@@ -16,9 +17,6 @@ const account = new Account(
     accountAddress,
     privateKey
 );
-
-
-
 
 // read abi of Test contract
 const { abi: testAbi } = await provider.getClassAt(flipTestAddress);
@@ -38,22 +36,18 @@ console.log("Initial balance =", reqId.toString());
 // With Cairo 1 contract, the result value is in bal1, as bigint.
 
 
-const resApprove = await wethTestContract.approve(flipTestAddress, cairo.uint256(7700000000000000000));
-await provider.waitForTransaction(resApprove.transaction_hash);
+const resApprove = await wethTestContract.approve(flipTestAddress, cairo.uint256(77000000000000000000));
+// await provider.waitForTransaction(resApprove.transaction_hash);
 
-// const allowance = await wethTestContract.allowance(accountAddress, flipTestAddress);
-// console.log("Allowance =", allowance.toString()); 
+const allowance = await wethTestContract.allowance(accountAddress, flipTestAddress);
+console.log("Allowance =", allowance.toString()); 
 
-// const tokenWETHMaxBettable = "150000000000000000"
-// const tokenWETHMinBettable = "0"
-// const tokenSupportTx = await flipTestContract.set_token_support(tokenWETHIdentifier, tokenWETHAddress, tokenWETHMaxBettable, tokenWETHMinBettable);
+const setFinalizerTx = await flipTestContract.set_finalizer(finalizer);
+await provider.waitForTransaction(setFinalizerTx.transaction_hash);
 
-
-// const myCall = flipTestContract.populate("issue_request", [1, 100000000, 0, tokenWETHIdentifier]);
 const res = await flipTestContract.issue_request(cairo.uint256(5), cairo.uint256(77000000000000000), 0, shortString.encodeShortString(tokenWETHIdentifier));
 const issueReqRes = await provider.waitForTransaction(res.transaction_hash);
 console.log('Status:', issueReqRes.execution_status);
-
 
 reqId = await flipTestContract.get_next_request_id();
 console.log("Initial balance =", reqId.toString()); 
@@ -72,16 +66,19 @@ function getConfig(network) {
             // Assign values based on the specified network
             privateKey = config[network].STARKNET_PRIVATE_KEY;
             accountAddress = config[network].STARKNET_ACCOUNT_ADDRESS;
+            provider = new RpcProvider({ nodeUrl: config[network].INFURA_KEY });
         } catch (error) {
             console.error('Error reading config file:', error);
             process.exit(1);
         }
     }
-    if (network == "TESTNET") {
-         provider = new RpcProvider({ sequencer: { network: constants.NetworkName.SN_GOERLI } }) // for testnet
-    }
     else {
-         provider = new RpcProvider({ sequencer: { network: constants.NetworkName.SN_MAIN } }) // for testnet
+        if (network === "TESTNET") {
+            nodeUrl = constants.NetworkName.SN_GOERLI; // for testnet
+        } else {
+            nodeUrl = constants.NetworkName.SN_MAIN; // for mainnet
+        }
+        provider = new RpcProvider({ sequencer: { network: nodeUrl } });
     }
     return { privateKey, accountAddress, provider};
 }
