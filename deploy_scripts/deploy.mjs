2ab795ee97039dda0dc3c547dc2643996a127d87
partychad <partychad@protonmail.com>
2023-12-05 16:24:27 +0100

    :recycle: Refactored token variables from WETH to ETH in deploy script.

--- deploy_scripts/deploy.mjs
+++ deploy_scripts/deploy.mjs
@@ -5,15 +5,14 @@ import { execSync } from 'child_process';
 import fs from 'fs';
 
 
-const tokenWETHAddress = '0x034e31357d1c3693bda06d04bf4c51557514ECed5A8e9973bDb772f7fB978B36'
-const tokenWETHIdentifier = "WETH"
-const tokenWETHMaxBettable = "150000000000000000"
-const tokenWETHMinBettable = "9900000000000000"
+const tokenETHAddress = '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7'
+const tokenETHIdentifier = "ETH"
+const tokenETHMaxBettable = "4500000000000000"
+const tokenETHMinBettable = "900000000000000"
 
 const treasuryAddress = "0x31c66f44d8455e5cac18b7016476685af8bd78bf4128cb5375ff2c45bc137c2";
 const ownerAddress = "0x31c66f44d8455e5cac18b7016476685af8bd78bf4128cb5375ff2c45bc137c2";
 const finalizerAddress = "0x003CF8aa30dA7aDB1D5a28687BF6802d80Bb8A02FBC9A4142020EC8309D7aaD0";
-const argentWalletAddress = "0x016C5242381A0FF101B239bf872462a0e63B70d9c94ca493EAa606ed0eA1baE1";
 const flipFeePercentage = 5;
 
 const absolutePath = "../target/dev/"
@@ -49,7 +48,7 @@ console.log("FlipBlob Contract Class Hash =", deployResponse.declare.class_hash)
 console.log('✅ FlibBlob Contract connected at =', myTestContract.address);
 myTestContract.connect(account);
 
-const tokenSupportTx = await myTestContract.set_token_support(shortString.encodeShortString(tokenWETHIdentifier), tokenWETHAddress, tokenWETHMaxBettable, tokenWETHMinBettable);
+const tokenSupportTx = await myTestContract.set_token_support(shortString.encodeShortString(tokenETHIdentifier), tokenETHAddress, tokenETHMaxBettable, tokenETHMinBettable);
 const tokenSupportReceipt = await provider.waitForTransaction(tokenSupportTx.transaction_hash);
 console.log('Status:', tokenSupportReceipt.execution_status);
 
@@ -57,10 +56,10 @@ console.log('Status:', tokenSupportReceipt.execution_status);
 // const transferOwnershipReceipt = await provider.waitForTransaction(transferOwnershipTx.transaction_hash);
 // console.log('Status:', transferOwnershipReceipt.execution_status);
 
-console.log(`Token support set for ${tokenWETHAddress} with the name ${tokenWETHIdentifier}. The max bettable in Wei is : ${tokenWETHMaxBettable}`);
+console.log(`Token support set for ${tokenETHAddress} with the name ${tokenETHIdentifier}. The max bettable in Wei is : ${tokenETHMaxBettable}`);
 console.log("Make sure that: ");
 console.log(`1. Treasury is funded -> ${treasuryAddress} `);
-console.log(`2. Treasury has approved the FlipBlob contract for spenditure for ${tokenWETHIdentifier} -> ${tokenWETHAddress}`);
+console.log(`2. Treasury has approved the FlipBlob contract for spenditure for ${tokenETHIdentifier} -> ${tokenETHAddress}`);
 console.log(`3. Finalizer is funded -> ${finalizerAddress}`);
 console.log("4. Finalizer is set in the backend.")
 
