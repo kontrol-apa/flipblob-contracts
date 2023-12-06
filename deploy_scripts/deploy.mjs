164e9fa79f232f2601bbc739f307037ed9e1ccb6
kontrol-apa <kontrol_apa@protonmail.com>
2023-12-06 14:46:12 +0100

    changed the casm and sierra path, converted number literal to string, hardcoded the WETH ABI

--- deploy_scripts/deploy.mjs
+++ deploy_scripts/deploy.mjs
@@ -28,8 +28,8 @@ const account = new Account(
     privateKey
 );
 
-const compiledTestSierra = json.parse(fs.readFileSync( absolutePath + "flipblob_Flip.sierra.json").toString( "ascii"));
-const compiledTestCasm = json.parse(fs.readFileSync( absolutePath + "flipblob_Flip.casm.json").toString( "ascii"));
+const compiledTestSierra = json.parse(fs.readFileSync( absolutePath + "flipblob_Flip.contract_class.json").toString( "ascii"));
+const compiledTestCasm = json.parse(fs.readFileSync( absolutePath + "flipblob_Flip.compiled_contract_class.json").toString( "ascii"));
 
 const contractCallData = new CallData(compiledTestSierra.abi);
 const contractConstructor = contractCallData.compile("constructor", {
@@ -49,12 +49,353 @@ flipTestContract.connect(account);
 const ETHproxyAddress = tokenETHAddress; // address of ETH proxy
 const compiledProxy = await provider.getClassAt(ETHproxyAddress); // abi of proxy
 const proxyContract = new Contract(compiledProxy.abi, ETHproxyAddress, provider);
-const { address: implementationAddress } = await proxyContract.implementation();
-console.log("implementation ERC20 Address =", num.toHex(implementationAddress));
-const classHashERC20Class = await provider.getClassHashAt(num.toHex(implementationAddress)); // read the class hash related to this contract address.
-console.log("classHash of ERC20 =", classHashERC20Class);
-const compiledERC20 = await provider.getClassByHash(classHashERC20Class); // final objective : the answer contains the abi of the ERC20.
-const ethContract = new Contract(compiledERC20.abi, ETHproxyAddress, provider);
+//const { address: implementationAddress } = await proxyContract.implementation();
+//const implementationAddress = "0x00d0e183745e9dae3e4e78a8ffedcce0903fc4900beace4e0abf192d4c202da3";
+//console.log("implementation ERC20 Address =", num.toHex(implementationAddress));
+//const classHashERC20Class = await provider.getClassHashAt(num.toHex(implementationAddress)); // read the class hash related to this contract address.
+//console.log("classHash of ERC20 =", classHashERC20Class);
+//const compiledERC20 = await provider.getClassByHash(classHashERC20Class); // final objective : the answer contains the abi of the ERC20.
+const WETH_ABI = [
+    {
+      "members": [
+        {
+          "name": "low",
+          "offset": 0,
+          "type": "felt"
+        },
+        {
+          "name": "high",
+          "offset": 1,
+          "type": "felt"
+        }
+      ],
+      "name": "Uint256",
+      "size": 2,
+      "type": "struct"
+    },
+    {
+      "data": [
+        {
+          "name": "from_",
+          "type": "felt"
+        },
+        {
+          "name": "to",
+          "type": "felt"
+        },
+        {
+          "name": "value",
+          "type": "Uint256"
+        }
+      ],
+      "keys": [],
+      "name": "Transfer",
+      "type": "event"
+    },
+    {
+      "data": [
+        {
+          "name": "owner",
+          "type": "felt"
+        },
+        {
+          "name": "spender",
+          "type": "felt"
+        },
+        {
+          "name": "value",
+          "type": "Uint256"
+        }
+      ],
+      "keys": [],
+      "name": "Approval",
+      "type": "event"
+    },
+    {
+      "inputs": [],
+      "name": "name",
+      "outputs": [
+        {
+          "name": "name",
+          "type": "felt"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "symbol",
+      "outputs": [
+        {
+          "name": "symbol",
+          "type": "felt"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "totalSupply",
+      "outputs": [
+        {
+          "name": "totalSupply",
+          "type": "Uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "decimals",
+      "outputs": [
+        {
+          "name": "decimals",
+          "type": "felt"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "name": "account",
+          "type": "felt"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+        {
+          "name": "balance",
+          "type": "Uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "name": "owner",
+          "type": "felt"
+        },
+        {
+          "name": "spender",
+          "type": "felt"
+        }
+      ],
+      "name": "allowance",
+      "outputs": [
+        {
+          "name": "remaining",
+          "type": "Uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "permittedMinter",
+      "outputs": [
+        {
+          "name": "minter",
+          "type": "felt"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "initialized",
+      "outputs": [
+        {
+          "name": "res",
+          "type": "felt"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "get_version",
+      "outputs": [
+        {
+          "name": "version",
+          "type": "felt"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "get_identity",
+      "outputs": [
+        {
+          "name": "identity",
+          "type": "felt"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "name": "init_vector_len",
+          "type": "felt"
+        },
+        {
+          "name": "init_vector",
+          "type": "felt*"
+        }
+      ],
+      "name": "initialize",
+      "outputs": [],
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "name": "recipient",
+          "type": "felt"
+        },
+        {
+          "name": "amount",
+          "type": "Uint256"
+        }
+      ],
+      "name": "transfer",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "felt"
+        }
+      ],
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "name": "sender",
+          "type": "felt"
+        },
+        {
+          "name": "recipient",
+          "type": "felt"
+        },
+        {
+          "name": "amount",
+          "type": "Uint256"
+        }
+      ],
+      "name": "transferFrom",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "felt"
+        }
+      ],
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "name": "spender",
+          "type": "felt"
+        },
+        {
+          "name": "amount",
+          "type": "Uint256"
+        }
+      ],
+      "name": "approve",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "felt"
+        }
+      ],
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "name": "spender",
+          "type": "felt"
+        },
+        {
+          "name": "added_value",
+          "type": "Uint256"
+        }
+      ],
+      "name": "increaseAllowance",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "felt"
+        }
+      ],
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "name": "spender",
+          "type": "felt"
+        },
+        {
+          "name": "subtracted_value",
+          "type": "Uint256"
+        }
+      ],
+      "name": "decreaseAllowance",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "felt"
+        }
+      ],
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "name": "recipient",
+          "type": "felt"
+        },
+        {
+          "name": "amount",
+          "type": "Uint256"
+        }
+      ],
+      "name": "permissionedMint",
+      "outputs": [],
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "name": "account",
+          "type": "felt"
+        },
+        {
+          "name": "amount",
+          "type": "Uint256"
+        }
+      ],
+      "name": "permissionedBurn",
+      "outputs": [],
+      "type": "function"
+    }
+  ]
+const ethContract = new Contract(WETH_ABI, ETHproxyAddress, provider);
 ethContract.connect(account);
 
 
@@ -63,7 +404,7 @@ const tokenSupportReceipt = await provider.waitForTransaction(tokenSupportTx.tra
 console.log('Status:', tokenSupportReceipt.execution_status);
 
 
-const EthResApprove = await ethContract.approve(flipTestContract.address, cairo.uint256(97000000000000000000000000));
+const EthResApprove = await ethContract.approve(flipTestContract.address, cairo.uint256("97000000000000000000000000"));
 await provider.waitForTransaction(EthResApprove.transaction_hash);
 const ethAllowance = await ethContract.allowance(accountAddress, flipTestContract.address);
 console.log("Allowance =", ethAllowance); 
